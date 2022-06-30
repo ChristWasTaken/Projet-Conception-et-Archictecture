b7cc8a5b0b2f863af82c441b43066b117f04602e
mallard1007 <melanieallard.1007@gmail.com>
2022-06-29 20:06:51 -0400

    melanie retrait de codes en commentaires dans le Coordonnateur

--- src/main/java/coordonnateur/Coordonnateur.java
+++ src/main/java/coordonnateur/Coordonnateur.java
@@ -60,57 +60,5 @@ public class Coordonnateur {
         return facadeBillet.consulterBilletParId(id);
     }
 
-//-------------------------------------------------------------------------------------------------------
-//Gab ok pour enlever
-//Mel ok pour enlever
-
-//        PackPres packPres = new PackPres();
-//        RegistreUsagerTech registreUsagerTech = RegistreUsagerTech.getInstance();
-//        ProjetDTO projet = new ProjetDTO(1, "ProjetTopSecret", LocalDate.of(2022,05,28), LocalDate.of(2022,05,29));
-//        CompteUsagerTechDTO usager = new CompteUsagerTechDTO(1, "Alain", "mdp", "email");
-
-
-//        FacadeServices facadeServices = new FacadeServices();
-//        facadeServices.creerProjet(projet);
-//        facadeServices.assignerUsagerTech(1,1);
-//
-//        FacadeCompteUsager facadeCompteUsager = new FacadeCompteUsager();
-//        facadeCompteUsager.creerCompteUsagerTech(usager);
-//
-//
-//        // Créer une catégorie de billet
-//        String categorieBillet = "Anomalie";
-//        //Ajout de la catégorie au ProjetDTO avant de l'envoyer à la facade
-//        projet.getRegistreCategories().ajouterUneCategorie(categorieBillet);
-//        facadeServices.ajoutDeCategorie(projet);
-//
-//        BilletDTO billetDto = new BilletDTO(1,"Ouvert","Urgent","demandeur@gmail.com",
-//                "Notes 1","Description1",
-//                RegistreCategories.getRegistreCategorieInstance().recupererCategorie("Anomalie"),
-//                LocalDate.now());
-//
-//        int idBillet = facadeServices.creerBillet(billetDto);
-//
-//        System.out.println(idBillet);
-//         billetDto = facadeServices.chercherParIdBillet(idBillet);
-//
-//        changerEtatBillet (c'est fucked up que le main soit dans le coordonateur. En attendant je fais mes calls ici moi aussi)
-//
-//        facadeServices.changerEtatBillet();
-//        facadeServices.afficherRegistreBillet();
-//        //consulterEtatBillet
-//        System.out.println(facadeServices.consulterDetailBillet(1));
-//
-//        //assigner un usager à un billet
-//        CompteUsagerTech usager2 = new CompteUsagerTech(2, "Toto", "mdp", "email@hotmail.com");
-//        billetDto.setIdUsagerTechAssigne(2);
-//        billetDto.setIdBillet(1);
-//        billetDto.setDateAssigne(LocalDate.now());
-//        billetDto.setDescription("Description2");
-////        System.out.println(billetDto);
-//        facadeServices.assignerBillet(billetDto);
-//        facadeServices.afficherRegistreBillet();
-
-
 
 }
